8f1f050639038ebf2b8cb94c3f40449b8798b47f
MSchiavi <mmschiav@buffalo.edu>
2020-05-15 20:04:13 -0400

    cleaned up some code and deleted a css file

--- react-quantum/src/App.js
+++ react-quantum/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PubNubReact from 'pubnub-react';
+import Swal from "sweetalert2";  
+import shortid  from 'shortid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+
+
+  render(){
+    <div>
+      hi
     </div>
-  );
+  }
 }
 
 export default App;
