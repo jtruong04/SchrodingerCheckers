e97c79f405da1685b1de619beed55c47d305f693
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2020-05-17 20:28:16 -0400

    Learning Flask and SQLAlch. Got the API to connect to test database! Need now to write the configuration to interface with front end

---
+++ quantum-api/src/SQLAlch.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template, jsonify
+from flask_restful import Resource, Api
+from markupsafe import escape
+from sqlalchemy import Column, create_engine, VARCHAR
+import urllib.parse
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    usr_name = Column(VARCHAR(length=255), primary_key=True)
+    pwd = Column(VARCHAR(length=255))
+
+
+if __name__ == '__main__':
+    '''
+    Test Routine
+    '''
+    # Set up SQL Alchemy Engine and ORM
+    sql_user = 'root'
+    sql_pwd = urllib.parse.quote_plus("<>")
+    engine = create_engine(('mysql://'
+                            + sql_user + ':' + sql_pwd
+                            + '@maybegames.dev:3306/QuantumCatDummy'))
+    session = sessionmaker()
+    session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    sess = session()
+
+    # Initiate Flask
+    app = Flask(__name__)
+    app.config["DEBUG"] = True
+
+
+    @app.route('/ApiTest', methods=['GET'])
+    def home():
+        user_info = sess.query(Users)
+        user_info = user_info.filter(Users.usr_name == 'John')[0].pwd
+
+        return user_info
+
+
+    app.run()
