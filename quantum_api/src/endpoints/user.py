7a0540fddf2c698b42d677b8c07a87ff93d1b748
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2020-06-15 20:49:06 -0400

    now i am done

--- quantum_api/src/endpoints/user.py
+++ quantum_api/src/endpoints/user.py
@@ -1,11 +1,38 @@
 from flask import Blueprint, jsonify, request
 from ..tables.user_table import Users
 from ..tables.games_table import Games
-import pprint
+from passlib.hash import sha256_crypt
 
 # User endpoint
 
 user = Blueprint('User', __name__)
+
+def salt_me(pwd):
+    """ Salts a password for upload to database
+
+    Arguments:
+        pwd {string} -- non-salted password supplied by user
+
+    Returns:
+        string -- password salted and encrypted
+    """
+
+    salted = sha256_crypt.encrypt(pwd)
+    return salted
+
+
+def verify_password(usr_given, from_db):
+    """ routine to check if password supplied by user matches salted password in db.
+
+    Arguments:
+        usr_given {string} -- password supplied by user, raw.
+        from_db {string} -- salted, and encrypted password from db
+
+    Returns:
+        boolean -- indicates if the two passwords match
+    """
+
+    return sha256_crypt.verify(usr_given, from_db)
 
 
 @user.route('/', methods=["GET"])
