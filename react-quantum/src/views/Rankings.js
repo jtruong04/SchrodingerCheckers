2ae6df263e96142a4079e0d3fec1aa8861e20733
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2020-06-19 21:12:22 -0400

    Ranking Page Fixes #41

--- react-quantum/src/views/Rankings.js
+++ react-quantum/src/views/Rankings.js
@@ -9,17 +9,66 @@ TODO: Everything. The page is pretty much blank right now
 */
 
 import React from "react";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import axios from 'axios';
 
 class Rankings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      top_players: []
+     
     };
   }
 
-  render() {
-    return <h1>Hello, this is the rankings page</h1>;
+  componentDidMount() {
+    axios.get('/rankings')
+      .then(res => {
+        const top_players = res.data;
+        this.setState({ top_players });
+      })
   }
+
+  renderTableData() {
+    return this.state.top_players.map((player, index) => {
+       const { id, name, age, email } = player //destructuring
+       return (
+          <tr key={id}>
+             <td>{id}</td>
+             <td>{name}</td>
+             <td>{age}</td>
+             <td>{email}</td>
+          </tr>
+       )
+    })
+ }
+
+ renderTableHeader() {
+  let header = Object.keys(this.state.top_players[0])
+  return header.map((key, index) => {
+     return <th key={index}>{key.toUpperCase()}</th>
+  })
+}
+
+ render() {
+  return (
+     <div>
+        <h1 id='title'>React Dynamic Table</h1>
+        <table id='students'>
+           <tbody>
+              {this.renderTableData()}
+           </tbody>
+        </table>
+     </div>
+  )
+}
+
+
+
 }
 
 export default Rankings;
