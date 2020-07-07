0bec8da196985d4ea368440ef74ad113a31dc355
Michael J. P. Morse <7416190+mjpmorse@users.noreply.github.com>
2020-07-06 20:00:11 -0400

    split node into proper nodes.

--- quantum_api/src/endpoints/api_endpoint.py
+++ quantum_api/src/endpoints/api_endpoint.py
@@ -16,40 +16,67 @@ api = Blueprint('api', __name__)
 def foo():
     return jsonify('public_games')
 
-@api.route('/public_games', methods=["GET", "POST", "DELETE", "PATCH"])
+@api.route('/games', methods=["GET", "POST"])
 def public_game():
-    """ End point connection to public games table. 
-        GET returns all games, DELETE removes a game by id,
-        POST adds a new game, PATCH updates game status
+  """ End point connection to public games table. 
+      GET returns all games POST adds a new game.
 
-        POST:
-            -- parameters (json), 200:
-              "host_name": string, name of location or host
-              "is_open": boolean 0 1, true if game is open to public
-            -- return (json), 200:
-              "host_name": string, name of location or host
-              "is_open": boolean, true if game is open to public
-              "creation_time": string, datetime of game creation
-              "id": game id, auto increments on table
-            -- return json(Exception), 500:
-              "Exception": error message / exception 
-        GET:
-            -- parameters:
-              NONE
-            -- return (json, list), 200:
-              "host_name": string, name of location or host
-              "is_open": boolean 0 1, true if game is open to public
-              "creation_time": string, datetime of game creation
-              "id": game id, auto increments on table
-            -- return json(Exception), 500:
-              "Exception": error message / exception 
-        DELETE: 
-            -- parameters (json):
-              "id": int or string, table id of game to delete
-            -- return json, 200:
-              "id"
-            -- return json(Exemption), 500:
-               "Exception": error message / exception   
+      POST:
+          -- parameters (json), 200:
+            "host_name": string, name of location or host
+            "is_open": boolean 0 1, true if game is open to public
+          -- return (json), 200:
+            "host_name": string, name of location or host
+            "is_open": boolean, true if game is open to public
+            "creation_time": string, datetime of game creation
+            "id": game id, auto increments on table
+          -- return json(Exception), 500:
+            "Exception": error message / exception 
+      GET:
+          -- parameters:
+            NONE
+          -- return (json, list), 200:
+            "host_name": string, name of location or host
+            "is_open": boolean 0 1, true if game is open to public
+            "creation_time": string, datetime of game creation
+            "id": game id, auto increments on table
+          -- return json(Exception), 500:
+            "Exception": error message / exception 
+  """
+  if request.method == 'GET':
+
+      try:
+          all_games = PublicGames.getAll()
+          all_g = []
+          for u in all_games:
+              dictret = dict(u.__dict__)
+              dictret.pop('_sa_instance_state', None)
+              all_g.append(dictret)
+          return jsonify(all_g), 200
+      except Exception as e:
+          return jsonify({"Exception": str(e)}), 500
+
+  elif request.method == 'POST':
+      data = request.json
+      host_name = data["host_name"]
+      is_open = bool(data["is_open"])
+      creation_time = datetime.now()
+      new_game = PublicGames(host_name=host_name, 
+                              is_open=is_open, creation_time=creation_time)
+      try:
+          _id = PublicGames.post(new_game)._id
+          return_game = {"host_name": host_name,
+                          "is_open": is_open,
+                          "creation_time": creation_time,
+                          "id": _id }
+          return jsonify(return_game), 200
+      except Exception as e:
+          return jsonify({"Exception": str(e)}), 500
+
+@api.route('/games/<int:id>', methods=["PATCH"])
+def game_patch(id):
+  """ End point connection to public games table. 
+      Update a game
         PATCH:
             -- parameters (json):
               "id": int or string, table id of game to update
@@ -60,62 +87,39 @@ def public_game():
               "creation_time": string, datetime of game creation
               "id": game id, auto increments on table
             -- return json(Exception), 500:
-              "Exception": error message / exception 
-                  
+              "Exception": error message / exception            
     """
-    
-    if request.method == 'GET':
+  _id = id
+  data = request.json
+  is_open = bool(data["is_open"])
+  try:
+    game = PublicGames.patch(_id, {'is_open': is_open})
+    return_game = {"host_name": game.host_name,
+                      "is_open": game.is_open,
+                      "creation_time": game.creation_time,
+                      "id": game._id }
+    return jsonify(return_game), 200
+  except Exception as e:
+    return jsonify({"Exception": str(e)}), 500
 
-        try:
-            all_games = PublicGames.getAll()
-            all_g = []
-            for u in all_games:
-                dictret = dict(u.__dict__)
-                dictret.pop('_sa_instance_state', None)
-                all_g.append(dictret)
-            return jsonify(all_g), 200
-        except Exception as e:
-            return jsonify({"Exception": str(e)}), 500
+@api.route('/games/<int:id>/DELETE', methods=["DELETE"])
+def game_delete(id):
+  """ End point connection to public games table. 
+        DELETE a game
+        DELETE: 
+            -- parameters (json):
+              "id": int or string, table id of game to delete
+            -- return json, 200:
+              "id"
+            -- return json(Exemption), 500:
+               "Exception": error message / exception                     
+    """
 
-    elif request.method == 'POST':
-        data = request.json
-        host_name = data["host_name"]
-        is_open = bool(data["is_open"])
-        creation_time = datetime.now()
-        new_game = PublicGames(host_name=host_name, 
-                               is_open=is_open, creation_time=creation_time)
-        try:
-            _id = PublicGames.post(new_game)._id
-            return_game = {"host_name": host_name,
-                           "is_open": is_open,
-                           "creation_time": creation_time,
-                           "id": _id }
-            return jsonify(return_game), 200
-        except Exception as e:
-            return jsonify({"Exception": str(e)}), 500
-    
-    elif request.method == 'DELETE':
-        data = request.json
-        _id = int(data["id"])
-        try:
-            PublicGames.delete(_id)
-            return jsonify(_id), 200
-        except Exception as e:
-            return jsonify({"Exception": str(e)}), 500
-
-    elif request.method == 'PATCH':
-        data = request.json
-        _id = int(data["id"])
-        is_open = bool(data["is_open"])
-        try:
-          game = PublicGames.patch(_id, {'is_open': is_open})
-          return_game = {"host_name": game.host_name,
-                           "is_open": game.is_open,
-                           "creation_time": game.creation_time,
-                           "id": game._id }
-          return jsonify(return_game), 200
-        except Exception as e:
-          return jsonify({"Exception": str(e)}), 500
-
+  _id = id
+  try:
+      PublicGames.delete(_id)
+      return jsonify(_id), 200
+  except Exception as e:
+      return jsonify({"Exception": str(e)}), 500
 
 
