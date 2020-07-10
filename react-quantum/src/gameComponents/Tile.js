ff11d5fe61d96188379142a532bc42f9d9e0cc08
John Truong <jtruong@buffalo.edu>
2020-07-10 02:41:58 -0400

    Features: Tile flip animation

--- react-quantum/src/gameComponents/Tile.js
+++ react-quantum/src/gameComponents/Tile.js
@@ -4,23 +4,43 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import grey from '@material-ui/core/colors/grey';
 import clsx from 'clsx';
-import { Paper } from '@material-ui/core';
+// import { Paper } from '@material-ui/core';
 // List of props available:
 // props.tile : tile data
 const useStyles = makeStyles({
+    scene: (props) => ({
+        width: `${100 / props.size - 2}%`,
+        padding: `0 0 calc(${100 / props.size - 2}%) 0`,
+        margin: '1%',
+        borderRadius: '10%',
+
+        perspective: '600%',
+        overflow: 'hidden',
+    }),
     tile: (props) => ({
         width: `${100 / props.size - 2}%`,
         padding: `0 0 calc(${100 / props.size - 2}%) 0`,
         margin: '1%',
-        position: 'relative',
-        overflow: 'hidden',
         borderRadius: '10%',
+        transition: 'transform 1s',
+        transformStyle: 'preserve-3d',
+        // transformOrigin: 'center right',
     }),
+    tileFace: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        backfaceVisibility: 'hidden',
+    },
     blackTile: {
         background: grey[900],
+        transform: 'rotateY(180deg)',
     },
     whiteTile: {
         background: grey[50],
+    },
+    isFlipped: {
+        transform: 'rotateY(-180deg)',
     },
 });
 
@@ -32,14 +52,17 @@ function Tile(props) {
     };
 
     return (
-        <Paper
+        <div
             elevation={3}
             className={clsx(
                 classes.tile,
-                props.state ? classes.blackTile : classes.whiteTile
+                props.state ? classes.isFlipped : null
             )}
             onClick={handleClick}
-        ></Paper>
+        >
+            <div className={clsx(classes.tileFace, classes.blackTile)}></div>
+            <div className={clsx(classes.tileFace, classes.whiteTile)}></div>
+        </div>
     );
 }
 
